4e78f854cffbfe024982720105a34b75f0afe320
Edward Evans <elevans@users.noreply.github.com>
2017-08-19 12:01:45 -0500

    Updated plotting
    Changed to ggplot style and reverted s=50 as that seems to be the ideal size.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Python-Scripts
 
-Note that clustering is based off of min_samples and not entirely eps (i.e. epsilon)
+Note that clustering is based off of min_samples and not entirely eps (i.e. epsilon).
+
+Ideal values
+eps = 100
+min_samples = 5
--- dbscan_lev_distance.py
+++ dbscan_lev_distance.py
@@ -69,9 +69,10 @@ print("DBSCAN Core indicies: \n", dbscan_model.core_sample_indices_)
 print("DBSCAN Labels: \n", dbscan_model.labels_)
 
 # Plot output
+plt.style.use('ggplot')
 plot_title_user_in = input("Enter plot title: ")
 plot_file_name_user_in = input("Enter output file name: ")
 fig, ax = plt.subplots(figsize=(10,8))
 ax.set_title(plot_title_user_in, fontsize=14)
-ax.scatter(dataset_pairwise[:,0], dataset_pairwise[:,1], c=dbscan_model.labels_, s=15, alpha=0.9, cmap=plt.cm.Set1)
+ax.scatter(dataset_pairwise[:,0], dataset_pairwise[:,1], c=dbscan_model.labels_, s=50, alpha=0.9, cmap=plt.cm.Set1)
 plt.savefig(plot_file_name_user_in + ".png")
