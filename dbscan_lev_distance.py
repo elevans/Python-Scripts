398370d9e84ade65633959d06fd8be1059572b83
Edward Evans <elevans@users.noreply.github.com>
2017-08-20 17:33:33 -0500

    Fig size update
    Increased figure size output

--- dbscan_lev_distance.py
+++ dbscan_lev_distance.py
@@ -47,6 +47,8 @@ def read_file (input_file):
 def lev_distance(s, u, v):
     return levenshtein(s[int(u)], s[int(v)])
 
+
+
 # Allows for user to define input file.
 user_file_path = input("Enter file path: ")
 dataset = read_file(user_file_path)
@@ -63,16 +65,19 @@ dataset_pairwise = pairwise_distances(dataset_reshape, metric=partial(lev_distan
 dbscan_model = cluster.DBSCAN(eps=eps_user_in, min_samples=min_samples_user_in, metric='euclidean', algorithm='brute').fit(dataset_pairwise)
 
 # Print parameters and DBSCAN output values
+
 print("Dataset Levenshtein output: \n", dataset_pairwise)
 print("DBSCAN Model: ", dbscan_model)
 print("DBSCAN Core indicies: \n", dbscan_model.core_sample_indices_)
 print("DBSCAN Labels: \n", dbscan_model.labels_)
 
+
 # Plot output
 plt.style.use('ggplot')
 plot_title_user_in = input("Enter plot title: ")
 plot_file_name_user_in = input("Enter output file name: ")
-fig, ax = plt.subplots(figsize=(10,8))
+fig, ax = plt.subplots(figsize=(15,12))
 ax.set_title(plot_title_user_in, fontsize=14)
 ax.scatter(dataset_pairwise[:,0], dataset_pairwise[:,1], c=dbscan_model.labels_, s=50, alpha=0.9, cmap=plt.cm.Set1)
 plt.savefig(plot_file_name_user_in + ".png")
+
