bc228964b1f5eafcf9e3cc8a894f17f7b70c1b33
Edward Evans <elevans@users.noreply.github.com>
2017-08-24 15:37:50 -0500

    Added gating and plotting functions
    Redesigned this a bit so i can just feed it .fcs files and it'll output ane name properly each graph that i want.

--- flowcytometry_cell_cycle_analysis.py
+++ flowcytometry_cell_cycle_analysis.py
@@ -8,15 +8,33 @@ import FlowCytometryTools
 
 from FlowCytometryTools import test_data_dir, test_data_file
 from FlowCytometryTools import FCMeasurement
+from FlowCytometryTools import ThresholdGate, PolyGate
 from pylab import *
 
-def generate_plots(X, Y, title, filename):
-    figure()
-    sample.plot([X, Y])
-    plt.savefig(filename)
+# Note that the names here are hard coded because I know what the output names are.
+# These can be changed to user inputed names easily if necessary.
+def generate_plots(filename):
+    figure(figsize=(10,10))
+    sample.plot(['FSC-A', 'SSC-A'], cmap=cm.Blues, colorbar=True)
+    plt.savefig('FSC-A_SSC-A_' + filename)
+    plt.clf()
+    sample.plot(['FSC-A', 'FSC-H'], cmap=cm.Blues, colorbar=True)
+    plt.savefig('FSC-A_FSC-H_' + filename)
+    plt.clf()
+    sample.plot(['CyChrome-W', 'CyChrome-A'], cmap=cm.Blues, colorbar=True)
+    plt.savefig('PI-W_PI-A_' + filename)
+    plt.clf()
+    sample.plot(['FSC-A', 'SSC-A'], gates=[live_dead_polygate], cmap=cm.Blues, colorbar=True)
+    plt.savefig('FSC-A_SSC-A_polygate_' + filename)
+    plt.clf()
+    live_dead_gated_sample.plot(['FSC-A', 'SSC-A'], cmap=cm.Blues, colorbar=True)
+    plt.savefig('Live_cells_' + filename)
+    plt.clf()
+    live_dead_gated_sample.plot(['FSC-A','FSC-H'], gates=[singles_polygate], cmap=cm.Blues, colorbar=True)
+    plt.savefig('Single_cells_' + filename)
+    plt.clf()
 
 # datadir points to directory of data, input_file points to specific FCS file
-#input_dir = test_data_dir
 input_file = raw_input("Enter FCS file location: ")
 
 # Load the flow data
@@ -25,15 +43,17 @@ data = sample.data
 
 # Transform datasets
 
+# Gating samples
+live_dead_polygate = PolyGate([(80000,50000),(120000,120000),(90000,130000),(70000,60000)],['FSC-A','SSC-A'], region='in', name='live')
+live_dead_gated_sample = sample.gate(live_dead_polygate)
+singles_polygate = PolyGate([(80000,0),(100000,65000),(75000,60000),(70000,0)],['FSC-A','FSC-H'], region='in', name='singles')
+
 # Print channel information
 print sample.channels
 
 # Print the number of events in the data
 print 'Number of events in file: ', data.shape[0]
 
-user_x = raw_input("X axis to plot: ")
-user_y = raw_input("Y axis to plot: ")
+# Save plots
 user_filename = raw_input("Enter plot file name: ")
-
-# Show plot
-generate_plots(user_x, user_y, user_filename)
+generate_plots(user_filename)
