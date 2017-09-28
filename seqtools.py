ae5193f051b5bc507ece4f469b5e7317153f6435
Edward Evans <elevans@users.noreply.github.com>
2017-09-28 16:43:51 -0500

    Added remove_duplicates

--- seqtools.py
+++ seqtools.py
@@ -1,3 +1,5 @@
+from progressbar import update_progress
+
 class ReadSequenceFile:
 
     def fasta(self, input_file):
@@ -15,7 +17,6 @@ class ReadSequenceFile:
 
         for line in content:
             if ">" in line:
-
                 seq_dict[seq_name] = concat_sequence
 
                 # Reset seq_name to current sequence and rest concat_sequence to empty.
@@ -27,23 +28,28 @@ class ReadSequenceFile:
 
         # Add last key value pair to dictionary
         seq_dict[seq_name] = concat_sequence        
-
         del seq_dict[""]
 
         return seq_dict
 
+    #def nexus(self, input_file):
+
+     #   seq_dict = {}
+
+      #  return seq_dict
+
 class WriteSequenceFile:
 
-    def fasta(self, output_filename, seq_list):
+    def fasta(self, output_filename, input_seq_list):
 
         i = 0
 
         with open(output_filename, 'w') as f:
-            while i < len(seq_list):
+            while i < len(input_seq_list):
                 seq_name = str(i)
                 seq_name_write = ">"+ seq_name + "\n"
                 f.write(seq_name_write)
-                seq_data = seq_list[i] + "\n"
+                seq_data = input_seq_list[i] + "\n"
                 f.write(seq_data)
                 i += 1
 
@@ -52,9 +58,9 @@ class WriteSequenceFile:
         
 class ParseSequences:
 
-    def remove_bad_seqs_DNA(self, seq_dict):
+    def remove_bad_seqs_DNA(self, input_seq_dict):
         
-        v = list(seq_dict.values())
+        v = list(input_seq_dict.values())
 
         initial_count = len(v)
 
@@ -62,14 +68,15 @@ class ParseSequences:
 
         # If sequences/strings containing any of the following characters, remove them from the sequence list.
         while i < len(v):
-
             line = v[i]
+            bad_chars = set('UWSMKRYBDHVNuwsmkrybdhvn')
 
-            if "N" in line:
+            if any((c in bad_chars) for c in line):
                 v.pop(i)
-          
-            elif "Y" in line:
-                v.pop(i)
+                print(line + " removed " + "index: " + str(i))
+                i = 0
+            else:
+                pass
 
             i += 1
 
@@ -81,26 +88,26 @@ class ParseSequences:
 
         return v
     
-    def remove_bad_seqs_protein(self, seq_dict):
+    def remove_bad_seqs_protein(self, input_seq_dict):
         
-        v = list(seq_dict.values())
-
+        v = list(input_seq_dict.values())
         initial_count = len(v)
 
         i = 0
 
         # If sequences/strings containing any of the following characters, remove them from the sequence list.
         while i < len(v):
-
             line = v[i]
+            bad_chars = set('Xx')
 
-            if "*" in line:
+            if any((c in bad_chars) for c in line):
                 v.pop(i)
-          
-            elif "X" in line:
-                v.pop(i)
+                print(line + " removed " + "index: " + str(i))
+                i = 0 # trick is here ;)
+            else:
+                pass
 
-            i += 1
+            i = i + 1
 
         final_count = len(v)
         removed_count = initial_count - final_count
@@ -110,50 +117,55 @@ class ParseSequences:
 
         return v
 
-    def site_count_DNA(self, seq_list, site_number):
-
-        i = 0
+    def site_count_DNA(self, input_seq_list, site_number):
 
         nuc_pos = int(site_number) - 1
-
+        i = 0
+     
         nuc_A = 0
         nuc_G = 0
         nuc_C = 0
         nuc_T = 0
 
-        while i < len(seq_list):
-
-            line = seq_list[i]
-
+        while i < len(input_seq_list):
+            line = input_seq_list[i]
             char = line[nuc_pos]
 
             if "A" == char:
-
                 nuc_A += 1
-
             elif "G" == char:
-
                 nuc_G += 1
-
             elif "C" == char:
-
                 nuc_C += 1
-
             elif "T" == char:
-
                 nuc_T += 1
 
             i += 1
 
-        seq_total = len(seq_list)
+        input_seq_total = len(input_seq_list)
 
-        nuc_A_percent = 100 * (nuc_A/seq_total)
-        nuc_G_percent = 100 * (nuc_G/seq_total)
-        nuc_C_percent = 100 * (nuc_C/seq_total)
-        nuc_T_percent = 100 * (nuc_T/seq_total)
+        nuc_A_percent = 100 * (nuc_A/input_seq_total)
+        nuc_G_percent = 100 * (nuc_G/input_seq_total)
+        nuc_C_percent = 100 * (nuc_C/input_seq_total)
+        nuc_T_percent = 100 * (nuc_T/input_seq_total)
 
         print("A: " + str(nuc_A) + " ," + str(nuc_A_percent) + " %")
         print("G: " + str(nuc_G) + " ," + str(nuc_G_percent) + " %")
         print("C: " + str(nuc_C) + " ," + str(nuc_C_percent) + " %")
         print("T: " + str(nuc_T) + " ," + str(nuc_T_percent) + " %")
 
+    def remove_duplicates(self, input_seq_list):
+
+        # Extract uniques and place them into a new list
+        raw_list = input_seq_list
+        uniques_list = []
+        [uniques_list.append(x) for x in raw_list if x not in uniques_list]
+
+        # Count the number of duplicate sequences removed
+        initial_count = len(raw_list)
+        updated_count = len(uniques_list)
+        removed_count = initial_count - updated_count
+
+        print (str(removed_count) + " duplicate sequences removed")
+        print ("Dataset size: ", len(uniques_list))        
+        return uniques_list
