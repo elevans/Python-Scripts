57b2c414592a97488b588945f64d8240237c4bed
Edward Evans <elevans@users.noreply.github.com>
2017-09-29 13:48:37 -0500

    Progress bar

---
+++ progressbar.py
@@ -0,0 +1,27 @@
+import time, sys
+
+# update_progress() : Displays or updates a console progress bar
+## Accepts a float between 0 and 1. Any int will be converted to a float.
+## A value under 0 represents a 'halt'.
+## A value at 1 or bigger represents 100%
+## credit:  Brain Khuu https://stackoverflow.com/questions/3160699/python-progress-bar
+## to use include update_progress(i/100) inside your loop. i = the iterator and 100 the number of iterations so something like len(list)
+
+def update_progress(progress):
+    barLength = 10 # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = "error: progress var must be float\r\n"
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Done...\r\n"
+    block = int(round(barLength*progress))
+    text = "\rPercent complete: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
