33e26f645bd58c686b9999ce37997bd7a7976f21
Edward Evans <elevans@users.noreply.github.com>
2017-10-08 15:54:11 -0500

    Updated pheno clustering

--- seq_pheno_clustering.py
+++ seq_pheno_clustering.py
@@ -10,17 +10,21 @@ from scipy.cluster.hierarchy import linkage
 from scipy.spatial import distance
 from treetools import ConvertTree, Dendrogram
 
+# Initialize objects
+tree_converter = ConvertTree()
+dendro_worker = Dendrogram()
+
 # Allows for user to define input file.
 user_tree_file = input("Enter tree file: ")
 
 # Convert input newick tree to a distance matrix (dmat) and a scipy linked matrix (schlink).
-tree_dmat, tree_schlink = ConvertTree.newick(user_tree_file)
-tree_labels = Dendrogram.extract_leaf_labels_newick(user_tree_file)
+tree_dmat, tree_schlink = tree_converter.newick(user_tree_file)
+tree_labels = dendro_worker.extract_leaf_labels_newick(user_tree_file)
 
 # Plot the dendrogram - use this to check if the input tree has the same topology as
 # the conveted tree.
 dendro_output_file_name = input("Enter output dendrogram file name: ")
-Dendrogram.save_dendrogram(tree_schlink, tree_labels, dendro_output_file_name)
+dendro_worker.save_dendrogram(tree_schlink, tree_labels, dendro_output_file_name)
 
 # Print distance matrix and scipy matrix to confirm successful converstion
 print('Tree distance matrix: ')
