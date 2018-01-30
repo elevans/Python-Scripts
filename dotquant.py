a0a30693c544023de4b5bec0e8d487222061ed40
Edward Evans <echnoviru@gmail.com>
2018-01-30 16:20:18 -0600

    Added docstrngs, user set vals and other misc thingys.

--- dotquant.py
+++ dotquant.py
@@ -1,49 +1,97 @@
 from math import sqrt
 from skimage.feature import blob_log
 from skimage.color import rgb2gray
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 def load_image (image_path):
 
-    """
+    """ Opens images (.tif, .png, .jpg)
 
+        retruns: input image, input image converted to a numpy array
     """
 
     img = Image.open(image_path)
     img_np = np.array(img)
     img_np_gray = rgb2gray(img_np)
 
-    return img_np_gray
+    return img, img_np_gray
 
-def compute_radii (input_image, image_path):
+def detect_blobs (input_image_original, input_image_np_gray, image_path):
 
+    """ Detects blobs in converted image (image is converted to numpy array)
+
+        Generates figure which can be saved and number of blobs identified
+
+        returns: null
     """
 
-    """
-    blobs_log = blob_log(input_image, max_sigma=0.2, num_sigma=10, threshold=0.2)
+    # Compute/detect blobs in image
+    blobs_log = blob_log(input_image_np_gray, max_sigma=max_sigma_val, num_sigma=num_sigma_val, threshold=threshold_val)
     blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
 
+    # Console results
     print("Detected " + str(len(blobs_log)) + " punctae.")
+
+    # Label the image with the number of punctae identified
+    # Note: Other operating systems (Windows and OSX) may not have the FreeSans font.  Change the font as necessary for the system.
+    image_draw = ImageDraw.Draw(input_image_original)
+    image_font = ImageFont.truetype("FreeSans.ttf", 60)
+    image_draw.text((0,0), str(len(blobs_log)) + " punctae", (255,255,255), font=image_font)
     
     fig, ax = plt.subplots(1, 1, figsize=(15, 9), sharex=True, sharey=True, subplot_kw={'adjustable': 'box-forced'})
     ax.set_title(image_path)
-    ax.imshow(input_image, interpolation='nearest')
+    ax.imshow(input_image_original, interpolation='nearest')
 
     blob_radii = []
 
+    # Add circles to image
     for blob in blobs_log:
         y, x, r = blob
         blob_radii.append(r)
-        c = plt.Circle((x, y), r, color='red', linewidth=2, fill=False)
+        c = plt.Circle((x, y), r, color='yellow', linewidth=2, fill=False)
         ax.add_patch(c)
 
     #print(blob_radii)
     plt.show()
 
+def set_user_values ():
+
+    print("Enter values.  Hit enter to select default values")
+
+    # User set max_sigma value
+    usr_max_sigma = input("max_sigma value (default = 2.5):")
+    if usr_max_sigma is "":
+        usr_max_sigma = float(2.5)
+        print("max_sigma is set to default value 2.5")
+    else:
+        usr_max_sigma = float(usr_max_sigma)
+        print("max_sigma value is set to: " + str(usr_max_sigma))
+
+    # User set num_sigma value
+    usr_num_sigma = input("num_sigma value (default = 10.0):")
+    if usr_num_sigma is "":
+        usr_num_sigma = float(10)
+        print("num_sigma is set to default vaule 10.0")
+    else:
+        usr_num_sigma = float(usr_num_sigma)
+        print("num_sigma value is set to: " +  str(usr_num_sigma))
+
+    # Usesr set threshold value
+    usr_threshold = input("threshold value (default = 0.2):")
+    if usr_threshold is "":
+        usr_threshold = float(0.2)
+        print("threshold is set to default value 0.2")
+    else:
+        usr_threshold = float(usr_threshold)
+        print("threshold value  is set to: " + str(usr_threshold))
+
+    return usr_max_sigma, usr_num_sigma, usr_threshold
 
 path = input("Enter image path: \n")
-data = load_image(path)
-compute_radii(data, path)
+
+max_sigma_val, num_sigma_val, threshold_val = set_user_values()
+original_image, gray_image = load_image(path)
+detect_blobs(original_image, gray_image, path)
