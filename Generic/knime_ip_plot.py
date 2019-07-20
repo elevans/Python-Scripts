550e93e282acce63d13a167fe2ce76f35b6a4012
Edward Evans <echnoviru@gmail.com>
2019-07-20 12:48:28 -0500

    Script for plotting data from KNIME

--- Generic/knime_ip_plot.py
+++ Generic/knime_ip_plot.py
@@ -9,42 +9,35 @@ user_response_no = ['n', 'N','no', 'NO','No']
 
 def ReadCSV (input_CSV):
 
-    # Reads CSV files and converts them to a panda DataFrame (long format)
+    # Reads CSV files and converts them to a panda DataFrame (long format), df_lf
+    df = pd.read_csv(input_CSV, sep=',').drop(['row ID'], axis='columns')
 
-    # read csv file and drop 'row ID' from dataframe. Note 'wf' = 'wide format'
-    df_wf = pd.read_csv(input_CSV, sep=',').drop(['row ID'], axis='columns')
-    
-    # extract measurements and timepoints
-    df_nuc = df_wf[['Mean_nucleus', 'Centroid Time', 'Label_Arr[1]']]
-    df_cyto = df_wf[['Mean_cytoplasm', 'Centroid Time', 'Label_Arr[1]']]
-
-    # add label column for nuc and cyto labels respectively
-    df_nuc['label'] = 'Nuc'
-    df_cyto['label'] = 'Cyto'
-
-    # rename columns
-    df_nuc.columns = ['signal', 'time', 'track','label']
-    df_cyto.columns = ['signal', 'time', 'track','label']
-
-    # create dataframe series
-    frames = [df_nuc, df_cyto]
-
-    # create new composite dataframe. Note 'lf' = 'long format'
-    df_lf = pd.concat(frames)
-    
-    return df_lf
+    return df
 
 def IntensityScatterPlot (input_df_lf):
 
-    user_input_intensity_plot = input('Generate intensity scatter plot? (y/n): ')
+    #user_input_intensity_plot = input('Generate intensity scatter plot? (y/n): ')
 
-    if user_input_intensity_plot in user_response_yes:
-        df_signal_sum = input_df_lf.groupby('track').sum().reset_index()
-        sns.relplot(x='track', y='signal', hue='track', data=df_signal_sum) # displays each tracks total nuc and cyto signal over total time.
-    else:
-        print('Intensity scatter plot skipped...')
+    df_col_names = list(input_df_lf.columns.values)
 
-    plt.show()
+    print ('Found the following channels: ')
+    for x in df_col_names:
+        if 'channel' in x:
+            print(x)
+        else:
+            None
+    
+    user_channel_select = 'mean_channel_' + input('Select channel number: ')
+    print(user_channel_select)
+
+    #if user_input_intensity_plot in user_response_yes:
+     #   df_signal_sum = input_df_lf.groupby('track').sum().reset_index()
+      #  print(df_signal_sum)
+        #sns.relplot(x='track', y=user_channel, hue='track', data=df_signal_sum) # displays each tracks total nuc and cyto signal over total time.
+    #else:
+     #   print('Intensity scatter plot skipped...')
+
+    #plt.show()
 
     return None
 
@@ -111,9 +104,10 @@ def WriteDataFrame (input_df_lf):
 
     return None
     
-input_data = input("Enter CSV file path: ")
+#input_data = input("Enter CSV file path: ")
+input_data = 'results.csv'
 data = ReadCSV(input_data)
 IntensityScatterPlot(data)
-data = TrackFilter(data)
-ShadeErrorPlot(data)
-WriteDataFrame(data)
+#data = TrackFilter(data)
+#ShadeErrorPlot(data)
+#WriteDataFrame(data)
