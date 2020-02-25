fca2bdc6c360b9c0603daf593701b0a51e731d98
Edward Evans <elevans2@wisc.edu>
2020-02-25 16:00:23 -0600

    Near complete script, writes good yuml files

--- Generic/yuml_script.py
+++ Generic/yuml_script.py
@@ -1,23 +1,19 @@
 import os
 
-def list_all_files(path):
-    for root, dirs, files in os.walk(path): # Outputs are lists
-        level = root.replace(path, '').count(os.sep) # Replace the path with '' and count the # of '/'.
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
+# TODO: Include file output
 
-def generate_yuml(path):
+def GenerateYuml(path):
     excluded_folders = ['main', 'java', '.ipynb_checkpoints', 'json', 'src', 'resources']
     excluded_files = ['.project', '.factorypath', '.classpath', '.envrc','pom.xml', '.mailmap', 'environment.yml', 'README.md', '.travis.yml', '.gitignore']
     ignored_dirs = ['.vscode', '.settings', '.travis', 'target', '.git']
+    folder_hold = ''
     yuml_output_list = []
+    included_files = ''
 
     for root, dirs, files in os.walk(path):
 
         # each loop is one file path/directory on the tree
+
         # setup variables
         filtered_files = []
         filtered_root = ''
@@ -35,7 +31,7 @@ def generate_yuml(path):
             filtered_root = root
 
         # base folder of current filtered root
-        folder_current = str(os.path.basename(filtered_root))
+        folder_base = str(os.path.basename(filtered_root))
 
         # computer folder level of current filtered root (counts /'s)
         level = filtered_root.replace(path, '').count(os.sep)
@@ -44,27 +40,59 @@ def generate_yuml(path):
         folder_up = os.path.dirname(filtered_root)
         folder_up = os.path.basename(folder_up)
 
-        # remove excluded folders
-        if folder_current in excluded_folders:
+        # check if folder up is in excluded list, if so assign previous loops base folder to up
+        if folder_up in excluded_folders:
+            folder_up = folder_hold
+        else:
+            pass
+
+        # remove excluded folders and hold last base folder value
+        if folder_base in excluded_folders:
             pass
         else:
-            if folder_current is '':
+            if folder_base is '':
                 pass
             else:
-                l = '[{}]-[{}]'.format(folder_up, folder_current)
+                folder_hold = folder_base
+                l = '[{}]-[{}]'.format(folder_up, folder_base)
                 yuml_output_list.append(l)
-        
+
+                if not files:
+                    pass
+                else:
+                    for g in files:
+                        if g in excluded_files:
+                            pass
+                        else:
+                            included_files += '{}\\n'.format(g)
+                
+                    if included_files is '':
+                        pass
+                    else:
+                        # convert files list string to yuml format
+                        k = '[{}]-[note: {}]'.format(folder_base, included_files)
+                        yuml_output_list.append(k)
+
+        # clear files string
+        included_files = ''
+
+    return yuml_output_list
+
+
+def WriteYumlFile(yuml):
+
     print('Writing file...')
 
     # write output file to yuml
     path = '/home/edward/Documents/Development/Repos/Personal/Python-Scripts/Generic/yuml_test.yuml'
     with open(path, 'w') as f:
         f.write('// {type:deployment}\n// {generate:true}\n\n')
-        for e in yuml_output_list:
+        for e in yuml:
             write_line = e + '\n'
             f.write(write_line)
 
         f.close()
 
-user_path = '/home/edward/Documents/Development/Repos/LOCI/tutorials'
-generate_yuml(str(user_path))
+user_path = '/home/edward/Documents/Development/Repos/LOCI/tutorials/howtos/src/main/java/howto/'
+yuml_result = GenerateYuml(str(user_path))
+WriteYumlFile(yuml_result)
