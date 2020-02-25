b842f6488f00735cfe3ec0ccb3a80e434787342d
Edward Evans <elevans2@wisc.edu>
2020-02-25 13:34:16 -0600

    Works for most folders, need fix for howtos dir

--- Generic/yuml_script.py
+++ Generic/yuml_script.py
@@ -10,57 +10,61 @@ def list_all_files(path):
             print('{}{}'.format(subindent, f))
 
 def generate_yuml(path):
-    excluded_folders = ['main', 'java', '.ipynb_checkpoints', 'metadata', 'json', 'META-INF', '.settings', 'src', 'target', '.git', '.vscode', '.travis']
-    excluded_files = ['.project', '.factorypath', '.classpath', '.envrc','pom.xml', '.mailmap',
-     'environment.yml', 'README.md', '.travis.yml', '.gitignore']
+    excluded_folders = ['main', 'java', '.ipynb_checkpoints', 'json', 'src', 'resources']
+    excluded_files = ['.project', '.factorypath', '.classpath', '.envrc','pom.xml', '.mailmap', 'environment.yml', 'README.md', '.travis.yml', '.gitignore']
+    ignored_dirs = ['.vscode', '.settings', '.travis', 'target', '.git']
+    yuml_output_list = []
 
     for root, dirs, files in os.walk(path):
 
         # each loop is one file path/directory on the tree
         # setup variables
         filtered_files = []
-
-        # compute folder level
-        level = root.replace(path, '').count(os.sep)
-
-        # base folder of root
-        folder_base = str(os.path.basename(root))
+        filtered_root = ''
         
-        # folder one leve up of root
-        # TODO: logic here to link back to the previous folder if folder up = exluded_files
-        folder_up = os.path.dirname(root)
+        # filteres out ignored dirs (e.g.  /.git)
+        for k in ignored_dirs:
+            if k in root:
+                root = ''
+            else:
+                pass
+        
+        if root is '':
+            pass
+        else:
+            filtered_root = root
+
+        # base folder of current filtered root
+        folder_current = str(os.path.basename(filtered_root))
+
+        # computer folder level of current filtered root (counts /'s)
+        level = filtered_root.replace(path, '').count(os.sep)
+
+        # get the folder above the current folder (i.e. base folder)
+        folder_up = os.path.dirname(filtered_root)
         folder_up = os.path.basename(folder_up)
 
-        # Remove excluded directories
-        if folder_base in excluded_folders:
-            folder_base = ''
-            continue
-        else:
-            pass
-
-        if not folder_base:
+        # remove excluded folders
+        if folder_current in excluded_folders:
             pass
         else:
+            if folder_current is '':
+                pass
+            else:
+                l = '[{}]-[{}]'.format(folder_up, folder_current)
+                yuml_output_list.append(l)
+        
+    print('Writing file...')
 
-            # Remove excluded files
-            
-            for item in files:
-                if item in excluded_files:
-                    None
-                else:
-                    filtered_files.append(item)
-                           
-        # compute yuml format
-        if level == 0:
-            print('[{}]'.format(folder_base))
-        else:
-            print('[{}]-[{}]'.format(folder_up, folder_base))
-            for i in filtered_files:
-                print('       ' + i)
+    # write output file to yuml
+    path = '/home/edward/Documents/Development/Repos/Personal/Python-Scripts/Generic/yuml_test.yuml'
+    with open(path, 'w') as f:
+        f.write('// {type:deployment}\n// {generate:true}\n\n')
+        for e in yuml_output_list:
+            write_line = e + '\n'
+            f.write(write_line)
+
+        f.close()
 
 user_path = '/home/edward/Documents/Development/Repos/LOCI/tutorials'
 generate_yuml(str(user_path))
-
-# output string proposal for yuml
-# line1 = '[{}]'.format(base)
-# line2 = '[{}]-[{}]'.format(base, connect)
