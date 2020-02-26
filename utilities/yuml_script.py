f9015f664ca9c725e1022a59c812f92ba90e580b
Edward Evans <elevans2@wisc.edu>
2020-02-26 11:36:59 -0600

    Working script for producing yuml digram files

--- utilities/yuml_script.py
+++ utilities/yuml_script.py
@@ -2,9 +2,9 @@ import os
 
 # TODO: Include file output
 
-def GenerateYuml(path):
+def GenerateYumlMapFolderFile(path):
     excluded_folders = ['main', 'java', '.ipynb_checkpoints', 'json', 'src', 'resources']
-    excluded_files = ['.project', '.factorypath', '.classpath', '.envrc','pom.xml', '.mailmap', 'environment.yml', 'README.md', '.travis.yml', '.gitignore']
+    excluded_files = ['.project', '.factorypath', '.classpath', '.envrc','pom.xml', '.mailmap', 'environment.yml', 'README.md', '.travis.yml', '.gitignore', 'plugins.config']
     ignored_dirs = ['.vscode', '.settings', '.travis', 'target', '.git']
     folder_hold = ''
     yuml_output_list = []
@@ -16,6 +16,7 @@ def GenerateYuml(path):
 
         # setup variables
         filtered_root = ''
+        loop_check = None
         
         # filteres out ignored dirs (e.g.  /.git)
         for k in ignored_dirs:
@@ -32,29 +33,26 @@ def GenerateYuml(path):
         # base folder of current filtered root
         folder_base = str(os.path.basename(filtered_root))
 
-        # computer folder level of current filtered root (counts /'s)
-        level = filtered_root.replace(path, '').count(os.sep)
-
         # get the folder above the current folder (i.e. base folder)
         folder_up = os.path.dirname(filtered_root)
-        folder_up = os.path.basename(folder_up)
+        folder_up = str(os.path.basename(folder_up))
 
         # check if folder up is in excluded list, if so assign previous loops base folder to up
         if folder_up in excluded_folders:
             folder_up = folder_hold
         else:
             pass
+
         
         # TODO: Add logic to capture files in folders that are in the excluded list.
         # remove excluded folders and hold last base folder value
+
         if folder_base in excluded_folders:
-            pass
+            folder_up = folder_hold # checks if the current folder is excluded and assigns non-excluded up-level folder
         else:
             if folder_base is '':
-
                 pass
             else:
-                print(files)
                 folder_hold = folder_base
                 l = '[{}]-[{}]'.format(folder_up, folder_base)
                 yuml_output_list.append(l)
@@ -67,6 +65,7 @@ def GenerateYuml(path):
                             pass
                         else:
                             included_files += '{}\\n'.format(g)
+                                
                 
                     if included_files is '':
                         pass
@@ -74,12 +73,42 @@ def GenerateYuml(path):
                         # convert files list string to yuml format
                         k = '[{}]-[note: {}]'.format(folder_base, included_files)
                         yuml_output_list.append(k)
+                        loop_check = True
+                        print('Current folder: {}\nFolder up: {}\n------------------------'.format(folder_base, folder_up))
+        
+        # captures wanted files in a folder in the excluded list, links to previous accepted folder.
+        if loop_check is True: 
+            pass
+        else:
+            if not files:
+                pass
+            else:
+                for h in files:
+                    if h in excluded_files:
+                        pass
+                    else:
+                        included_files += '{}\\n'.format(h)
+
+                if included_files is '':
+                    pass
+                else:
+                    if folder_up is '': # folder_up will be empty if root is empty due to ignored dir
+                        pass
+                    else:
+                        k = '[{}]-[note: {}]'.format(folder_up, included_files)
+                        yuml_output_list.append(k)
+                        print('Current folder: {}\nFolder up: {}\n------------------------'.format(folder_base, folder_up))
 
         # clear files string
         included_files = ''
 
     return yuml_output_list
 
+def GenerateYumlMapFolder(path):
+
+    # only output folder structure in yuml format
+
+    return
 
 def WriteYumlFile(yuml):
 
@@ -95,6 +124,6 @@ def WriteYumlFile(yuml):
 
         f.close()
 
-user_path = '/home/edward/Documents/Development/Repos/LOCI/tutorials/maven-projects/add-rois'
-yuml_result = GenerateYuml(str(user_path))
+user_path = '/home/edward/Documents/Development/Repos/LOCI/tutorials/howtos'
+yuml_result = GenerateYumlMapFolderFile(str(user_path))
 WriteYumlFile(yuml_result)
